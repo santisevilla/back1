c93b8cc5e210f2c5bf5cf3fbff79fcb1a651d8b2
Santiago Sevilla <63272703+santisevilla@users.noreply.github.com>
2022-07-05 18:39:48 -0300

    auth

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import characterRoutes from "../src/routes/characterRoutes.js";
 import movieRoutes from "../src/routes/movieRoutes.js";
 import genreRoutes from "../src/routes/genreRoutes.js";
-import authRoutes from "../src/routes/authRoutes.js";
+
 
 const app = express();
 
@@ -11,6 +11,5 @@ app.use(express.json());
 app.use("/characters", characterRoutes);
 app.use("/movies", movieRoutes);
 app.use("/genres", genreRoutes);
-app.use("/auth", authRoutes);
 
 export default app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import characterRoutes from "./characterRoutes.js";
 import movieRoutes from "./movieRoutes.js";
 import genreRoutes from "./genreRoutes.js";
+import userRoutes from "./userRoutes.js"
 
 const router = Router();
 
 router.use("/characters", characterRoutes);
 router.use("/movies", movieRoutes);
 router.use("/genres", genreRoutes);
+router.use("/users", userRoutes)
 
 export default router;
