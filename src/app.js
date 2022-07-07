ad90b05bc8030d3e215c68e231445bda42f79a48
Santiago Sevilla <63272703+santisevilla@users.noreply.github.com>
2022-07-07 20:49:55 -0300

    users

--- src/app.js
+++ src/app.js
@@ -2,17 +2,16 @@ import express from "express";
 import characterRoutes from "../src/routes/characterRoutes.js";
 import movieRoutes from "../src/routes/movieRoutes.js";
 import genreRoutes from "../src/routes/genreRoutes.js";
-import userRoutes from "../src/routes/userRoutes.js"
+import userRoutes from "../src/routes/userRoutes.js";
 
 const app = express();
 
 //middlewars
 app.use(express.json());
 
-
 app.use("/characters", characterRoutes);
 app.use("/movies", movieRoutes);
 app.use("/genres", genreRoutes);
-app.use("/users", userRoutes)
+app.use("/users", userRoutes);
 
 export default app;
